e056a5c92dba87b6d5f5309a6551bf18a792b883
WangYichen <72858135+WangYichen2000@users.noreply.github.com>
2022-09-18 23:24:47 +0800

    it can be run frequently.
    jiayou

--- TransCNN.py
+++ TransCNN.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch
+import numpy as np
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -8,6 +9,7 @@ class AddNorm(nn.Module):
         super(AddNorm, self).__init__()
         self.dropout = nn.Dropout(dropout)
         self.bn = nn.BatchNorm1d(normalized_shape)
+
     def forward(self, x, y):
         bn_output = self.bn(self.dropout(y.permute(1, 2, 0)) + x.permute(1, 2, 0))
         return bn_output.permute(2, 0, 1)
@@ -37,9 +39,12 @@ class CNN(nn.Module):
             nn.MaxPool2d(kernel_size=2, stride=2),
             CNNlayer(in_channels=15, out_channels=24, kernel_size=3),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            CNNlayer(in_channels=24, out_channels=36, kernel_size=3),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Flatten()
                     )
         self.fc = nn.Sequential(
-            nn.Linear(in_features=24, out_features=1),
+            nn.Linear(in_features=144, out_features=1),
             nn.Sigmoid()
         )
 
@@ -55,25 +60,28 @@ class TransCNN(nn.Module):
         self.attention = nn.MultiheadAttention(dim_model, num_heads)
         self.add = AddNorm(normalized_shape=(in_feature, ), dropout=0.1)
         self.fnn_layer = nn.Sequential(  # 注意力加残差之后直接卷积
-            nn.Linear(in_features=43, out_features=512),
+            nn.Linear(in_features=43, out_features=256),
             nn.ReLU(),
-            nn.Linear(in_features=512, out_features=43),
-            nn.Dropout(p=0.3, inplace=False),
+            nn.Linear(in_features=256, out_features=43),
+            nn.Dropout(p=0.3, inplace=False)
+
+        )
+        self.cnn = CNN()
 
     def forward(self, x):
         attention_output, _ = self.attention(x, x, x)
         add_output = self.add(x, attention_output)  # 残差连接+batchnorm
         linear_output = self.fnn_layer(add_output)
         fnn_output = self.add(add_output, linear_output)
-        return fnn_output
+        cnn_out = self.cnn(fnn_output.permute(1, 0, 2)[:, np.newaxis, :, :])
+        return cnn_out
 
 
 if __name__ == "__main__":
-    trans_model = TransCNN(in_feature=43, dim_model=43, num_layers=2, num_heads=1, seq_length=5).cuda()
+    trans_model = TransCNN(in_feature=43, dim_model=43, num_layers=2, num_heads=1, seq_length=5).to(device)
     cnn_model = CNN()
     test_x = torch.randn((41, 32, 43)).to(device)
     test_X_cnn = torch.randn((32, 1, 41, 43)).to(device)
     out_trans = trans_model(test_x)
     out_cnn = cnn_model(test_X_cnn)
-    print(out_trans.shape)
-    print('\n', out_trans)
+    print(out_trans.shape, out_cnn.shape)
